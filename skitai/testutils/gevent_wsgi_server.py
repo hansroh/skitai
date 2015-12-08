46e80cdb0e1cf350aef6af7fcb99b466c3360125
Hans Roh <hansroh@gmail.com>
2015-12-08 06:18:05 +0000

    linux

--- skitai/testutils/gevent_wsgi_server.py
+++ skitai/testutils/gevent_wsgi_server.py
@@ -6,9 +6,10 @@ from gevent.pywsgi import WSGIServer
 
 def application(environ, start_response):
 	start_response('200 OK', [('Content-Type', 'text/plain')])
-	f = open ("/var/local/skitaid-pub/test/static/test.htm", "rb")	
-	data = f.read ()
-	f.close ()
+	#f = open ("/var/local/skitaid-pub/test/static/test.htm", "rb")	
+	#data = f.read ()
+	#f.close ()
+	data = b"Pong"
 	return [data]
 
 print('Serving on 5001...')	
