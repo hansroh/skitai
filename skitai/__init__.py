4ace54f0dc92d7c8615d15c9811896dd79521691
Hans Roh <hansroh@gmail.com>
2016-09-23 15:57:04 +0900

    0.16.7 enalbe CONNECT method within HTTP2

--- skitai/__init__.py
+++ skitai/__init__.py
@@ -1,6 +1,6 @@
 # 2014. 12. 9 by Hans Roh hansroh@gmail.com
 
-VERSION = "0.16.8"
+VERSION = "0.16.7"
 version_info = tuple (map (lambda x: not x.isdigit () and x or int (x),  VERSION.split (".")))
 NAME = "SAE/%s.%s" % version_info [:2]
 
