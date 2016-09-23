587dca90ceef151a219f0eba98ac7f7de3ff32a4
Hans Roh <hansroh@gmail.com>
2016-09-23 20:15:26 +0900

    rename README.txt -> rst

--- skitai/__init__.py
+++ skitai/__init__.py
@@ -2,7 +2,7 @@
 
 VERSION = "0.16.7"
 version_info = tuple (map (lambda x: not x.isdigit () and x or int (x),  VERSION.split (".")))
-NAME = "SAE/%s.%s" % version_info [:2]
+NAME = "SWAE/%s.%s" % version_info [:2]
 
 import threading
 import sys
