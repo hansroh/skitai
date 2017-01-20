da46232754be23ac7f06b288ba3e4b3255bc7757
Hans Roh <hansroh@gmail.com>
2017-01-20 17:32:09 +0900

    fix merge conflicts

--- skitai/server/handlers/http2/request.py
+++ skitai/server/handlers/http2/request.py
@@ -9,23 +9,6 @@ class request (http_request.http_request):
 		self.scheme = scheme
 		self.stream_id = stream_id
 		self._is_promise = is_promise
-		
-<<<<<<< HEAD
-		self.logger = self.channel.server.server_logger
-		self.server_ident = self.channel.server.SERVER_IDENT
-		self.body = None
-		self.reply_code = 200
-		self.reply_message = ""		
-		self._split_uri = None
-		self._header_cache = {}
-		self.rbytes = 0
-		self.loadbalance_retry = 0
-		self.gzip_encoded = False		
-		
-		self.outgoing_producer = None
-=======
-		#self.outgoing_producer = None
->>>>>>> http2
 		self.depends_on = 0
 		self.weight = 0
 	
