c0038de8768bfa2e95b051f26f3e15e2f037680c
Hhans Roh <hansroh@gmail.com>
2017-08-12 22:33:52 +0900

    add test_promise.py

---
+++ tests/test_promise.py
@@ -0,0 +1,24 @@
+from skitai.server.wastuff import promise
+from confutil import rprint
+import time
+import random
+import pytest
+from mock import MagicMock
+
+def handler (promise, resp):
+	assert promise.fulfilled ()
+	assert not promise.settled ()
+	assert not promise.ready ()
+	promise ["b"] == "b"
+	promise.settle ()		
+	assert promise.settled ()
+	assert promise.ready ()	
+	promise.reject ('Error')
+	assert promise.rejected ()
+	
+def test_promise (wasc):
+	p = promise.Promise (wasc (), handler, a = "a")
+	assert p ["a"] == "a"
+	p._numreq = 1
+	p (MagicMock ())
+	
