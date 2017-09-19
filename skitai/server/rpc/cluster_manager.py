2e07c90a5364addc72fe6dec1b6e78f2cddeda0b
Hans Roh <hansroh@gmail.com>
2017-09-20 08:42:55 +0900

    fix cluster_manager.backen = True

--- skitai/server/rpc/cluster_manager.py
+++ skitai/server/rpc/cluster_manager.py
@@ -55,7 +55,7 @@ class ClusterManager:
 	object_timeout = 1200
 	maintern_interval = 60
 	# I cannot sure this is faster
-	backend = False
+	backend = True
 	
 	def __init__ (self, name, cluster, ssl = 0, access = None, logger = None):
 		self.logger = logger
