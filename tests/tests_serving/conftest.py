1028eeb5bc93634f130ca68c7a314bf011ff330b
Hans Roh <hansroh@gmail.com>
2018-04-16 21:07:53 +0000

    rename sub tests

--- tests/tests_serving/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from functools import partial
-import skitai
-
-@pytest.fixture
-def launch ():
-    return partial (skitai.launch, port = 30371, silent = False)
--- tests/tests_v1/test_proxy_handler.py
+++
@@ -1,9 +0,0 @@
-from confutil import rprint, assert_request
-import confutil
-import skitai
-import os
-from skitai.server import offline
-	
-def test_proxy_handler ():
-	ph = offline.install_proxy_handler ()
-	
