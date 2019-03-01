143160f20dd0df4055388f52696707e7da297f36
Hans Roh <hansroh@gmail.com>
2019-03-01 13:49:15 +0000

    upload to pypi

--- skitai/__init__.py
+++ skitai/__init__.py
@@ -1,6 +1,6 @@
 # 2014. 12. 9 by Hans Roh hansroh@gmail.com
 
-__version__ = "0.28.7.22"
+__version__ = "0.28.7.23"
 
 version_info = tuple (map (lambda x: not x.isdigit () and x or int (x),  __version__.split (".")))
 NAME = "Skitai/%s.%s" % version_info [:2]
