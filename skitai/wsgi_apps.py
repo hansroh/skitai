4cae3ab8d169315adad222bfe0c6b2832f6becfc
Hans Roh <hansroh@gmail.com>
2019-03-12 23:23:17 +0000

    chg max_post_body_size from 5M -> 1M

--- skitai/wsgi_apps.py
+++ skitai/wsgi_apps.py
@@ -9,10 +9,10 @@ from rs4 import attrdict
 from importlib import reload
 
 def set_default (cf):
-	cf.max_post_body_size = 5 * 1024 * 1024
-	cf.max_cache_size = 5 * 1024 * 1024
+	cf.max_post_body_size = 1 * 1024 * 1024
+	cf.max_cache_size = 1 * 1024 * 1024
 	cf.max_multipart_body_size = 20 * 1024 * 1024
-	cf.max_upload_file_size = 20000000
+	cf.max_upload_file_size = 20 * 1024 * 1024
 	
 def Config (preset = False):
 	cf = attrdict.AttrDict ()
