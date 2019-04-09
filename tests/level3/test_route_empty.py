7cda3015711c01515ca68cb4284f57c15387bffb
Hans Roh <hansroh@gmail.com>
2019-04-09 11:47:44 +0000

    add test_route_root.py

---
+++ tests/level3/test_route_empty.py
@@ -0,0 +1,39 @@
+from atila import Atila
+import confutil
+import skitai
+import asyncore
+import os
+from rs4 import jwt as jwt_
+import time
+
+def test_route_empty (app):
+    app._mount_option ["point"] = "/beta"
+
+    @app.route ("/")
+    def index2 (was, path = None):
+        return "Hello, World"
+
+    with app.test_client ("/", confutil.getroot ()) as cli:
+        resp = cli.get ("/beta")
+        assert resp.status_code == 301
+
+        resp = cli.get ("/beta/")        
+        assert resp.status_code == 200
+
+        resp = cli.get ("/beta/1")        
+        assert resp.status_code == 404
+
+
+    @app.route ("")
+    def index (was, path = None):
+        return "Hello, World"
+
+    with app.test_client ("/", confutil.getroot ()) as cli:
+        resp = cli.get ("/beta")
+        assert resp.text == "Hello, World"
+
+        resp = cli.get ("/beta/")        
+        assert resp.status_code == 200
+
+        resp = cli.get ("/beta/1")        
+        assert resp.status_code == 404
--- tests/level3/test_route_root.py
+++ tests/level3/test_route_root.py
@@ -18,4 +18,3 @@ def test_route_root (app, dbpath):
         
         resp = cli.get ("/hello")
         assert resp.text == "Hello, World"
-        
