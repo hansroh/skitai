04dbd0946a7fda6aa03ef6ef947f84713621e9c9
Hans Roh <hansroh@gmail.com>
2019-04-30 11:12:50 +0900

    add lukeys to alias

--- tests/level1/test_skitai.py
+++ tests/level1/test_skitai.py
@@ -14,8 +14,11 @@ def test_skitai (app):
     skitai.set_worker_critical_point ()
     
     skitai.deflu ("a", "b")
-    assert (skitai.dconf ["models-keys"] == ["a", "b"])
+    assert (skitai.dconf ["models-keys"] == {"a", "b"})
     
+    with pytest.raises (KeyError):
+        skitai.deflu ("a", "b")        
+
     if os.name != "posix":
         return
     
@@ -33,6 +36,9 @@ def test_skitai (app):
     assert skitai.dconf ['mount']["default"][0][1][0].endswith ('/bin/path/app.py')
     assert skitai.dconf ['mount']["default"][0][1][1] == 'app'
     
+    skitai.alias ("@core", skitai.DB_PGSQL, "postgres:4000Wkwkdaus@80.sns.co.kr/coin_core", ["c"])
+    assert (skitai.dconf ["models-keys"] == {"a", "b", "c"})
+
     if tfserver:
         pref = skitai.pref ()
         pref.config.tf_models = None
