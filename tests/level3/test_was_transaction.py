7f75240ed607201e3d699681377cda1e6b7aa6dc
Hans Roh <hansroh@gmail.com>
2019-07-30 08:37:40 +0000

    add was.sdb test

--- tests/level3/test_was_transaction.py
+++ tests/level3/test_was_transaction.py
@@ -2,7 +2,7 @@ import skitai
 import confutil
 import pprint
 
-def test_futures (app, dbpath):
+def test_db2 (app, dbpath):
     @app.route ("/")
     def index (was):
         with was.transaction ("@sqlite") as trx:
@@ -14,6 +14,21 @@ def test_futures (app, dbpath):
             d = trx.fetchall ()
                 
         return str (d)
+            
+    app.alias ("@sqlite", skitai.DB_SQLITE3, dbpath)    
+    with app.test_client ("/", confutil.getroot ()) as cli:
+        resp = cli.get ("/")
+        assert "RHAT" in resp.text
+
+
+def test_db3 (app, dbpath):
+    @app.route ("/")
+    def index (was):
+        with was.sdb ("@sqlite") as trx:
+            d = trx.execute ('SELECT * FROM stocks WHERE symbol=?', ('RHAT',)).fetch ()            
+        with was.sdb ("@sqlite") as trx:
+            d2 = trx.execute ('SELECT * FROM stocks WHERE symbol=?', ('RHAT',))
+        return str (d2.fetch ())
             
     app.alias ("@sqlite", skitai.DB_SQLITE3, dbpath)    
     with app.test_client ("/", confutil.getroot ()) as cli:
