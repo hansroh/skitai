741f3d8d7633b69e9a899f89076d03ffaf855dda
Hans Roh <hansroh@gmail.com>
2019-08-28 09:20:45 +0000

    protest

--- skitai/__init__.py
+++ skitai/__init__.py
@@ -1,6 +1,6 @@
 # 2014. 12. 9 by Hans Roh hansroh@gmail.com
 
-__version__ = "0.29.1.1"
+__version__ = "0.29.1.2"
 
 version_info = tuple (map (lambda x: not x.isdigit () and x or int (x),  __version__.split (".")))
 NAME = "Skitai/%s.%s" % version_info [:2]
