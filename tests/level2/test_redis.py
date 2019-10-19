c922dc73709b0c9cb493369aac30c5187b98c558
Hans Roh <hansroh@gmail.com>
2019-10-19 09:28:30 +0000

    update redis test

--- tests/level2/test_redis.py
+++ tests/level2/test_redis.py
@@ -3,13 +3,9 @@ from redis.exceptions import DataError
 import pytest
 
 def test_redis ():
-    redis = redisconn.Connection ()
-    
-    assert redis.pack_command ("PUSH", "someting") == [b'*2\r\n$4\r\nPUSH\r\n$8\r\nsometing\r\n']
-    assert redis.pack_command ("PUSH", 1) == [b'*2\r\n$4\r\nPUSH\r\n$1\r\n1\r\n']
+    pack_command = redisconn.Connection ().pack_command
+
+    assert pack_command ("PUSH", "someting") == [b'*2\r\n$4\r\nPUSH\r\n$8\r\nsometing\r\n']
+    assert pack_command ("PUSH", 1) == [b'*2\r\n$4\r\nPUSH\r\n$1\r\n1\r\n']
     with pytest.raises (DataError):
-        redis.pack_command ("PUSH", [1])
-    
-    
-    
-    
+        pack_command ("PUSH", [1])
