69b8c15c49f335d2a247ce1a4e4e37781d9d4178
Hans Roh <hansroh@gmail.com>
2019-10-26 05:31:27 +0000

    fix http3

--- skitai/http3_server.py
+++ skitai/http3_server.py
@@ -11,21 +11,21 @@ import skitai
 from errno import EWOULDBLOCK
 
 class http3_channel (https_server.https_channel):
-	def __init__ (self, server, data, addr):
-		http_server.http_channel.__init__(self, server, None, addr)
+    def __init__ (self, server, data, addr):
+        http_server.http_channel.__init__(self, server, None, addr)
         self.create_socket (socket.AF_INET, socket.SOCK_DGRAM)
-		self.connect (addr)
+        self.connect (addr)
 
 
 class http3_server (https_server.https_server):
     def create_socket (self, stack):
-		super ().create_socket (stack, socket.SOCK_DGRAM)
+        super ().create_socket (stack, socket.SOCK_DGRAM)
 
     def _serve (self, shutdown_phase = 2):
-		self.shutdown_phase = shutdown_phase
+        self.shutdown_phase = shutdown_phase
 
     def handle_connect(self):
-		pass
+        pass
 
     def handle_read (self):
         ret = self.recv (self.buffer_size)
@@ -39,18 +39,18 @@ class http3_server (https_server.https_server):
         try:
             return self.socket.recvfrom (buffer_size)
         except MemoryError:
-			lifetime.shutdown (1, 1.0)
+            lifetime.shutdown (1, 1.0)
 
-		except ssl.SSLError as why:
-			if why.errno == ssl.SSL_ERROR_WANT_READ:
-				try:
-					raise BlockingIOError
-				except NameError:
-					raise socket.error (EWOULDBLOCK)
-			# closed connection
-			elif why.errno in (ssl.SSL_ERROR_ZERO_RETURN, ssl.SSL_ERROR_EOF):
-				self.handle_close ()
-				return None
-			else:
-				raise
+        except ssl.SSLError as why:
+            if why.errno == ssl.SSL_ERROR_WANT_READ:
+                try:
+                    raise BlockingIOError
+                except NameError:
+                    raise socket.error (EWOULDBLOCK)
+            # closed connection
+            elif why.errno in (ssl.SSL_ERROR_ZERO_RETURN, ssl.SSL_ERROR_EOF):
+                self.handle_close ()
+                return None
+            else:
+                raise
 
