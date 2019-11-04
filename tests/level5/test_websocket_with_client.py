909530353f51f439ba7e62ea5e6dd846984f6d91
Hans Roh <hansroh@gmail.com>
2019-11-04 23:55:53 +0000

    fixing websocket for pypy #1

--- tests/level5/test_websocket_with_client.py
+++ tests/level5/test_websocket_with_client.py
@@ -8,60 +8,60 @@ import aquests
 from websocket import create_connection
 
 def test_websocket (launch):
-    with launch ("./examples/websocket.py") as engine:                
-        ws = create_connection("ws://127.0.0.1:30371/websocket/echo2")
-        ws.send("Hello, World")
-        result =  ws.recv()
-        assert result =="Welcome Client 0"        
-        result =  ws.recv()
-        assert result == "1st: Hello, World"        
-        result =  ws.recv()
-        assert result == "2nd: Hello, World"
-        ws.close()
-        
+    with launch ("./examples/websocket.py") as engine:
+        # test THREADSAFE ----------------------------------
         ws = create_connection("ws://127.0.0.1:30371/websocket/echo3")
         ws.send("Hello, World")
         result =  ws.recv()
-        assert result =="Welcome Client 0"        
+        assert result =="Welcome Client 0"
         result =  ws.recv()
-        assert result == "1st: Hello, World"        
+        assert result == "1st: Hello, World"
         result =  ws.recv()
         assert result == "2nd: Hello, World"
         ws.close()
-        
+
+        # test GROUPCHAT ---------------------------------- ----------------------------------
         ws = create_connection("ws://127.0.0.1:30371/websocket/chat2?room_id=1")
         ws.send("Hello, World")
         result =  ws.recv()
-        assert result =="Client 2 has entered"        
+        assert result =="Client 2 has entered"
         result =  ws.recv()
-        assert result == "Client 2 Said: Hello, World"        
-        
+        assert result == "Client 2 Said: Hello, World"
+
         ws2 = create_connection("ws://127.0.0.1:30371/websocket/chat2?room_id=1")
         ws2.send("Absolutely")
-                
         result =  ws2.recv()
-        assert result == "Client 3 Said: Absolutely"        
-        
+        assert result == "Client 3 Said: Absolutely"
         result =  ws.recv()
         assert result == "Client 3 Said: Absolutely"
-        
-        ws.close()        
+        ws.close()
         ws2.close()
 
-
-def test_websocket_flask (launch):
-    with launch ("./examples/websocket-flask.py") as engine:                
+        # test NOTHREAD ----------------------------------
         ws = create_connection("ws://127.0.0.1:30371/websocket/echo2")
         ws.send("Hello, World")
         result =  ws.recv()
-        assert result =="Welcome Client 0"        
+        assert result =="Welcome Client 0"
         result =  ws.recv()
-        assert result == "1st: Hello, World"        
+        assert result == "1st: Hello, World"
         result =  ws.recv()
         assert result == "2nd: Hello, World"
         ws.close()
- 
- 
+
+
+def test_websocket_flask (launch):
+    with launch ("./examples/websocket-flask.py") as engine:
+        ws = create_connection("ws://127.0.0.1:30371/websocket/echo2")
+        ws.send("Hello, World")
+        result =  ws.recv()
+        assert result =="Welcome Client 0"
+        result =  ws.recv()
+        assert result == "1st: Hello, World"
+        result =  ws.recv()
+        assert result == "2nd: Hello, World"
+        ws.close()
+
+
 def test_websocket (launch):
     with launch ("./examples/websocket.py") as engine:
         ws = create_connection("ws://127.0.0.1:30371/websocket/push")
@@ -69,7 +69,7 @@ def test_websocket (launch):
         result =  ws.recv()
         assert result == "you said: Hello, World"
         resp = engine.get ("/websocket/wspush")
-        assert resp.text == "Sent"        
+        assert resp.text == "Sent"
         result =  ws.recv()
-        assert result == "Item In Stock!"                
-        ws.close()       
+        assert result == "Item In Stock!"
+        ws.close()
