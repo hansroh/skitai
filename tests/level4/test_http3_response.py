4446fc12ca94a6f69001710894a5ff5aa9293746
Hans Roh <hansroh@gmail.com>
2019-11-04 21:30:56 +0000

    fix_http3_tests

--- tests/level4/test_http3_response.py
+++ tests/level4/test_http3_response.py
@@ -3,8 +3,9 @@ import os
 import socket
 import time
 from aquests.protocols.http3 import requests
+import sys
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_http2 (launch):
     serve = './examples/http3.py'
     with launch (serve, port = 30371, quic = 30371, ssl = True) as engine:
@@ -23,6 +24,9 @@ def test_http2 (launch):
         assert len (resp.text) == 1000006
 
 def test_http3 (launch):
+
+    if sys.version_info.major == 3 and sys.version_info.minor < 6:
+        return
     serve = './examples/http3.py'
     with launch (serve, port = 30371, quic = 30371, ssl = True) as engine:
         resp = engine.http3.get ('/hello?num=1')
--- tests/level4/test_http3_server_push.py
+++ tests/level4/test_http3_server_push.py
@@ -8,6 +8,8 @@ import aquests
 from aquests.protocols.http2.hyper import HTTPConnection
 
 def test_http3_server_push (launch):
+    if sys.version_info.major == 3 and sys.version_info.minor < 6:
+        return
     serve = './examples/http3.py'
     with launch (serve, port = 30371, quic = 30371, ssl = True) as engine:
         resp = engine.http3.get ('/promise')
