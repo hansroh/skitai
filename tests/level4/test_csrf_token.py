65cfc7025f090f906a1c61f2f5e1b19428369698
Hans Roh <hansroh@gmail.com>
2020-03-01 11:18:42 +0000

    update csrf token

--- tests/level4/test_csrf_token.py
+++ tests/level4/test_csrf_token.py
@@ -12,3 +12,12 @@ def test_app (launch):
         resp = engine.post ('/post', {'_csrf_token': csrf, 'a': 'b'})
         assert resp.status_code == 200
         assert resp.text == 'OK'
+
+        resp = engine.get ('/')
+        assert resp.status_code == 200
+        csrf = resp.text [5:].strip ()
+
+        resp = engine.post ('/post', {'_csrf_token': csrf, 'a': 'b'}, headers = {'X-CSRF-Token': csrf})
+        assert resp.status_code == 200
+        assert resp.text == 'OK'
+
