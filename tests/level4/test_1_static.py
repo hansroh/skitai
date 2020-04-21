042a5e3cabe0f2c51af3c64c364a02b4a60f888a
Hans Roh <hansroh@gmail.com>
2020-04-21 01:37:54 +0000

    force ordering tests

--- tests/level4/test_1_static.py
+++ tests/level4/test_1_static.py
@@ -4,9 +4,11 @@ IS_PYPY = platform.python_implementation() == 'PyPy'
 
 def test_app (launch):
     with launch ("./examples/app2.py") as engine:
+        # if IS_PYPY:
+        #     # i don't know why fail
+        #     return
         resp = engine.get ('/reindeer')
-        if not IS_PYPY:
-            assert resp.headers.get ('etag')
+        assert resp.headers.get ('etag')
         assert resp.headers.get ('content-type') == 'image/jpeg'
         assert resp.headers.get ('content-length') == '32772'
 
