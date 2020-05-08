e1a6f69c9bb0cff9abd55216af680bdfb1c1c9f7
Hans Roh <hansroh@gmail.com>
2020-05-08 11:30:21 +0000

    fix test_http3_response

--- tests/level4/test_http3_response.py
+++ tests/level4/test_http3_response.py
@@ -6,7 +6,7 @@ import sys
 
 def test_http2 (launch):
     serve = './examples/http3.py'
-    with launch (serve, port = 30371, quic = 30371, ssl = True) as engine:
+    with launch (serve, port = 30371, ssl = True) as engine:
         resp = engine.http2.get ('/hello?num=1')
         assert resp.text == 'hello'
 
