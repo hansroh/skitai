af24513a94b617b369b0f7dbb3281ab48b2cf427
Hans Roh <hansroh@gmail.com>
2020-06-21 11:46:42 +0000

    add pebble for pth.task

--- skitai/corequest/pth/executors.py
+++ skitai/corequest/pth/executors.py
@@ -5,6 +5,10 @@ import time
 from .task import Task
 import sys
 from rs4.psutil import kill
+import pebble
+from pebble import ProcessExpired
+from concurrent.futures import TimeoutError
+from ..httpbase.task import DEFAULT_TIMEOUT
 
 N_CPU = multiprocessing.cpu_count()
 
@@ -12,10 +16,10 @@ class ThreadExecutor:
     NAME = "thread"
     MAINTERN_INTERVAL = 10
 
-    def __init__ (self, workers = None, zombie_timeout = None, logger = None):
+    def __init__ (self, workers = None, default_timeout = None, logger = None):
         self.logger = logger
         self.workers = workers or N_CPU
-        self.zombie_timeout = zombie_timeout
+        self.default_timeout = default_timeout
         self.last_maintern = time.time ()
         self.lock = multiprocessing.Lock ()
         self.executor = None
@@ -30,7 +34,8 @@ class ThreadExecutor:
             return len (self.futures)
 
     def launch_executor (self):
-        self.executor = rs4.tpool (self.workers)
+        # self.executor = rs4.tpool (self.workers)
+        self.executor = pebble.ThreadPool (self.workers)
 
     def status (self):
         with self.lock:
@@ -38,7 +43,7 @@ class ThreadExecutor:
                 completions = self._dones,
                 timeouts = self._timeouts,
                 maintainables = len (self.futures),
-                zombie_timeout = self.zombie_timeout,
+                default_timeout = self.default_timeout,
                 workers = self.workers,
                 last_maintern = self.last_maintern,
                 activated = self.executor is not None,
@@ -51,25 +56,27 @@ class ThreadExecutor:
         for future in self.futures:
             if future.done ():
                 self._dones += 1
-                if future.exception ():
-                    try:
-                        raise future.exception ()
-                    except:
-                        self.logger.trace ()
+                try:
+                    future.result ()
+                except TimeoutError as error:
+                    self.logger ("{} task took longer than {} seconds: {}".format (self.NAME, error.args[1], future), 'error')
+                except ProcessExpired as error:
+                    self.logger ("{} task died unexpectedly with exit code {}: {}".format (self.NAME, error.exitcode, future), 'error')
+                except Exception as error:
+                    self.logger.trace ()
                 continue
 
             if self.no_more_request:
                 timeout = -1 # kill immediately
             else:
                 timeout = future.get_timeout ()
-                if timeout is None:
-                    timeout = self.zombie_timeout
 
             # timeout is 0 or None, it is infinite task
             if timeout is not None and future._started + timeout < now:
                 future.kill ()
                 self._timeouts += 1
                 self.logger ("try to kill zombie {} task: {}".format (self.NAME, future), 'warn')
+
             inprogresses.append (future)
         self.futures = inprogresses
 
@@ -80,15 +87,21 @@ class ThreadExecutor:
                 return
             self.maintern (time.time ())
             # if False, Py3.7 raise OSError: OSError: handle is closed
-            if sys.version_info [:2] >= (3, 9):
-                self.executor.shutdown (cancel_futures = True)
-            else:
-                self.executor.shutdown ()
+            # if sys.version_info [:2] >= (3, 9):
+            #     self.executor.shutdown (cancel_futures = True)
+            # else:
+            #     self.executor.shutdown ()
 
+            self.executor.stop ()
+            self.executor.close ()
             self.executor = None
             self.futures = []
 
             return len (self.futures)
+
+    def create_task (self, f, a, b, timeout):
+        # return self.executor.submit (f, *a, **b)
+        return self.executor.schedule (f, args = a, kwargs = b)
 
     def __call__ (self, was_id, f, *a, **b):
         with self.lock:
@@ -104,9 +117,12 @@ class ThreadExecutor:
         meta = {}
         timeout, filter = None, None
         if not a:
+            try:
+                timeout = b.pop ('timeout')
+            except KeyError:
+                timeout = self.default_timeout
+
             try: meta = b.pop ('meta')
-            except KeyError: pass
-            try: timeout = b.pop ('timeout')
             except KeyError: pass
             try: filter = b.pop ('filter')
             except KeyError: pass
@@ -115,7 +131,7 @@ class ThreadExecutor:
             b = b.get ('kwargs', b)
 
         meta ['__was_id'] = was_id
-        future = self.executor.submit (f, *a, **b)
+        future = self.create_task (f, a, b, timeout)
         wrap = Task (future, "{}.{}".format (f.__module__, f.__name__), meta = meta, filter = filter)
         timeout and wrap.set_timeout (timeout)
         self.logger ("{} task started: {}".format (self.NAME, wrap))
@@ -126,16 +142,20 @@ class ThreadExecutor:
 class ProcessExecutor (ThreadExecutor):
     NAME = "process"
     def launch_executor (self):
-        self.executor = rs4.ppool (self.workers)
+        # self.executor = rs4.ppool (self.workers)
+        self.executor = pebble.ProcessPool (self.workers)
+
+    def create_task (self, f, a, b, timeout = None):
+        return self.executor.schedule (f, args = a,  kwargs = b, timeout = timeout)
 
 # ------------------------------------------------------------------------
 
 class Executors:
-    def __init__ (self, workers = N_CPU, zombie_timeout = None, logger = None):
+    def __init__ (self, workers = N_CPU, default_timeout = DEFAULT_TIMEOUT, logger = None):
         self.logger = logger or screen_logger ()
         self.executors = [
-            ThreadExecutor (workers, zombie_timeout, self.logger),
-            ProcessExecutor (workers, zombie_timeout, self.logger)
+            ThreadExecutor (workers, default_timeout, self.logger),
+            ProcessExecutor (workers, default_timeout, self.logger)
         ]
 
     def status (self):
