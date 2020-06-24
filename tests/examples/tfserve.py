2762ac9734e520df75cab5f7d00d41877f6f53e2
Hans Roh <hansroh@gmail.com>
2020-06-24 10:37:00 +0000

    fix tfserver test

--- tests/examples/tfserve.py
+++ tests/examples/tfserve.py
@@ -17,11 +17,8 @@ def load_latest_model (model_name, subdir = None, per_process_gpu_memory_fractio
 if __name__ == "__main__":
     pref = skitai.pref ()
     pref.max_client_body_size = 100 * 1024 * 1024 # 100 MB
-    pref.debug = True
-    pref.use_reloader = True
     pref.access_control_allow_origin = ["*"]
     pref.config.tf_models = {}
     load_latest_model ("test", skitai.joinpath ("models"), 0.1)
     skitai.mount ("/", dnn, pref = pref)
     skitai.run (port = 5000, name = "tfms")
-
--- tests/level5/test_tfserving.py
+++ tests/level5/test_tfserving.py
@@ -9,15 +9,14 @@ Y = [0.0, 1.0]
 SEQLEN = 12
 
 def test_tfserver ():
-    try:
-        from dnn.predutil import TFServer
-        from dnn.tfserver import cli
-        import numpy as np
-    except ImportError:
-        return
-
     serve = "./examples/tfserve.py"
     with skitai.test_client (serve, port = 30371, silent = False):
+        try:
+            from dnn.predutil import TFServer
+            import numpy as np
+        except ImportError:
+            return
+
         s = TFServer ("http://127.0.0.1:30371", "test")
         y = s.predict (x = np.array ([X]), seq_length = np.array ([SEQLEN]))
         assert y.shape == (1, 2)
