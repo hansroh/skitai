cd15a1d27d3cb633a6376e5577de20fabacecb9a
Hans Roh <hansroh@gmail.com>
2020-09-13 12:46:05 +0000

    test4

--- tests/test-all.sh
+++ tests/test-all.sh
@@ -1,7 +1,5 @@
-# pytest level1
-# pytest level2
-# pytest level3
-# pytest level4
-# pytest level5
-
-pytest -s level4/test_launcher.py
+pytest level1
+pytest level2
+pytest level3
+pytest level4
+pytest level5
