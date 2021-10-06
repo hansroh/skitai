4044a8d5a358405cb5034c32049e872549a1255f
Hans Roh <hansroh@gmail.com>
2021-10-06 08:20:20 +0000

    add test_decorators

---
+++ tests/level3/test_decorators.py
@@ -0,0 +1,22 @@
+from atila import Atila
+import confutil
+import skitai
+from rs4 import asyncore
+import os
+from rs4.webkit import jwt as jwt_
+import time
+import route_guide_pb2
+import pytest
+
+def test_cli (app, dbpath, is_pypy):
+    def test (was):
+        was.request.g.target = 'World'
+
+    @app.route ("/")
+    @app.testpass_required (test)
+    def index (was):
+        return "Hello, {}".format (was.request.g.target)
+
+    with app.test_client ("/", confutil.getroot ()) as cli:
+        resp = cli.get ("/")
+        assert resp.text == "Hello, World"
