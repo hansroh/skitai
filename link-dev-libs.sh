4193bb940e77e46352b83852fbc83b051ddfe9b9
Hans Roh <hansroh@gmail.com>
2021-11-27 11:07:30 +0000

    rename libpub -> libs

--- link-dev-libs.sh
+++ link-dev-libs.sh
@@ -6,11 +6,11 @@ rm -rf /usr/local/lib/python3.6/dist-packages/skitai*
 rm -rf /usr/local/lib/python3.6/dist-packages/sqlphile*
 rm -rf /usr/local/lib/python3.6/dist-packages/tfserver*
 
-ln -s /home/ubuntu/libpub/atila/atila /usr/local/lib/python3.6/dist-packages/atila
-ln -s /home/ubuntu/libpub/atila-vue/atila_vue /usr/local/lib/python3.6/dist-packages/atila_vue
-ln -s /home/ubuntu/libpub/delune/delune /usr/local/lib/python3.6/dist-packages/delune
-ln -s /home/ubuntu/libpub/dnn/dnn /usr/local/lib/python3.6/dist-packages/dnn
-ln -s /home/ubuntu/libpub/rs4/rs4 /usr/local/lib/python3.6/dist-packages/rs4
-ln -s /home/ubuntu/libpub/skitai/skitai /usr/local/lib/python3.6/dist-packages/skitai
-ln -s /home/ubuntu/libpub/sqlphile/sqlphile /usr/local/lib/python3.6/dist-packages/sqlphile
+ln -s /home/ubuntu/libs/atila/atila /usr/local/lib/python3.6/dist-packages/atila
+ln -s /home/ubuntu/libs/atila-vue/atila_vue /usr/local/lib/python3.6/dist-packages/atila_vue
+ln -s /home/ubuntu/libs/delune/delune /usr/local/lib/python3.6/dist-packages/delune
+ln -s /home/ubuntu/libs/dnn/dnn /usr/local/lib/python3.6/dist-packages/dnn
+ln -s /home/ubuntu/libs/rs4/rs4 /usr/local/lib/python3.6/dist-packages/rs4
+ln -s /home/ubuntu/libs/skitai/skitai /usr/local/lib/python3.6/dist-packages/skitai
+ln -s /home/ubuntu/libs/sqlphile/sqlphile /usr/local/lib/python3.6/dist-packages/sqlphile
 ln -s /home/ubuntu/libpub/tfserver/tfserver /usr/local/lib/python3.6/dist-packages/tfserver
