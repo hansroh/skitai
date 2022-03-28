160b7fddf56f003c522d87e2047a431307134f7f
hansroh <hansroh@gmail.com>
2022-03-28 10:34:46 +0000

    add skitai.get_gpu_memory()

--- skitai/__init__.py
+++ skitai/__init__.py
@@ -297,6 +297,15 @@ def register_g (*names):
     return decorator
 register_cache_keys = register_states = register_g
 
+def get_gpu_memory ():
+    import subprocess as sp
+
+    _output_to_list = lambda x: x.decode ('ascii').split('\n')[:-1]
+    COMMAND = "nvidia-smi --query-gpu=memory.used --format=csv"
+    memory_free_info = _output_to_list(sp.check_output(COMMAND.split()))[1:]
+    memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
+    return memory_free_values
+
 #------------------------------------------------
 # Configure
 #------------------------------------------------
