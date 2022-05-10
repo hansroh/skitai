e046e3a5df45bc2d279af6452fadd8aa0e520576
hansroh <hansroh@gmail.com>
2022-05-10 01:56:41 +0000

    actviate py39

--- tests/level1/test_getopt.py
+++ tests/level1/test_getopt.py
@@ -2,4 +2,4 @@ import skitai
 import sys
 
 def test_getopt ():
-    opts, args = skitai.getopt ("shf:x", ["ssl", "debug", "origin=", "ff", "lf"])
+    opts, args = skitai.getopt ("shf:x", ["ssl", "debug", "origin=", "ff", "lf", "disable-pytest-warnings"])
