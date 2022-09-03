f90cc094039e809ceb74253c047d12f773a080de
hansroh <hansroh@gmail.com>
2022-09-03 02:27:46 +0000

    test

--- tests/test-all.sh
+++ tests/test-all.sh
@@ -2,7 +2,8 @@
 # pytest -x --ff $1 $2 $3 level1 && \
 # pytest -x --ff $1 $2 $3 level2 && \
 # pytest -x --ff $1 $2 $3 level3 && \
-pytest -x --ff -s $1 $2 $3 level4 && \
+# pytest -x --ff $1 $2 $3 level4 && \
 # pytest -x --ff $1 $2 $3 level4-1 && \
 # pytest -x --ff $1 $2 $3 level4-2 && \
 # pytest -x --ff $1 $2 $3 level5
+pytest -x --ff $1 $2 $3 level4
