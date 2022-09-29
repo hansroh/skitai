45b56f35541a3202a45d5d9422e77dfb481d2039
hansroh <hansroh@gmail.com>
2022-09-29 23:22:03 +0000

    fix RouteGuideStub

--- tests/level5/test_bidirectional.py
+++ tests/level5/test_bidirectional.py
@@ -5,7 +5,7 @@ try:
     from examples.services.route_guide_pb2 import RouteGuideStub
 except ImportError:
     from examples.services import route_guide_pb2_v3 as route_guide_pb2
-    from examples.services.route_guide_pb2_grpc import RouteGuideStub
+    from examples.services.route_guide_pb2_v3 import RouteGuideStub
 
 import os
 import time
