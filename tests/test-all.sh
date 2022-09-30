22386df5553927b4c275343804c3358eddbaea79
hansroh <hansroh@gmail.com>
2022-09-30 01:10:40 +0000

    update requirements

--- tests/test-all.sh
+++ tests/test-all.sh
@@ -1,11 +1,10 @@
-# pytest -x --ff $1 $2 $3 level0 && \
-# pytest -x --ff $1 $2 $3 level1 && \
-# pytest -x --ff $1 $2 $3 level2 && \
-# pytest -x --ff $1 $2 $3 level3 && \
-# pytest -x --ff $1 $2 $3 level4 && \
-# pytest -x --ff $1 $2 $3 level4-1 && \
-# pytest -x --ff $1 $2 $3 level4-2 && \
-# pytest -x --ff $1 $2 $3 level5
+pytest -x --ff $1 $2 $3 level0 && \
+pytest -x --ff $1 $2 $3 level1 && \
+pytest -x --ff $1 $2 $3 level2 && \
+pytest -x --ff $1 $2 $3 level3 && \
+pytest -x --ff $1 $2 $3 level4 && \
+pytest -x --ff $1 $2 $3 level4-1 && \
+pytest -x --ff $1 $2 $3 level4-2 && \
+pytest -x --ff $1 $2 $3 level5
 
-pytest -s -x --ff $1 $2 $3 level5/test_bidirectional.py
-pytest -s -x --ff $1 $2 $3 level5/test_grpc.py
+# pytest -s -x --ff $1 $2 $3 level5/test_bidirectional.py
