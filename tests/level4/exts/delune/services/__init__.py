d4963496df5ab5ba1c94b86527dfb74f5e9a1903
hansroh <hansroh@gmail.com>
2022-10-01 08:03:05 +0000

    refactor levle4 testing

--- tests/level4/exts/delune/services/__init__.py
+++ tests/level4/exts/delune/services/__init__.py
@@ -1,5 +1,18 @@
 
 def __mount__ (context, app, opts):
-    @app.route ('/delune-ext')
+    @app.route ("/")
     def index (context):
+        return '<h1>Custom Delune</h1>'
+
+    @app.route ('/delune-ext')
+    def index_ext (context):
         return 'delune-ext'
+
+    @app.route ('/delune-ext')
+    def index_ext (context):
+        return 'delune-ext'
+
+    @app.route ("/cols/<alias>/documents2/<_id>", methods = ["GET"])
+    @app.route ("/cols/<alias>/documents/<_id>", methods = ["GET"])
+    def get (context, alias, _id, nthdoc = 0):
+        return ""
