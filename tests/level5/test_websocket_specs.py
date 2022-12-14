dce4d0991be2cfc9f502cbf76cb5489db4a813ff
hansroh <hansroh@gmail.com>
2022-12-14 09:56:10 +0000

    update test

--- tests/level5/test_websocket_specs.py
+++ tests/level5/test_websocket_specs.py
@@ -64,7 +64,7 @@ def test_websocket_reporty (launch):
         ws.close()
 
 def test_websocket_reporty_async (launch, launch_dry):
-    with launch_dry ("./examples/websocket-spec.py") as engine:
+    with launch ("./examples/websocket-spec.py") as engine:
         ws = create_connection("ws://127.0.0.1:30371/websocket/reporty/async?a=AMBER")
         ws.send("Hello, World")
 
